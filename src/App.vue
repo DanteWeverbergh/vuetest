7becfcfa14b54c348cac3242d442d9bf44d279a1
{DanteWeverbergh} <dante.weverbergh@outlook.com>
2023-01-05 23:50:43 +0100

    :tada: language selector

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <LanguageSelector />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
+import LanguageSelector from "./components/LanguageSelector.vue";
 
 @Component({
   components: {
-    HelloWorld,
+    LanguageSelector,
   },
 })
 export default class App extends Vue {}
---
+++ src/components/LanguageSelector.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <select v-model="language">
+      <option value="en">English</option>
+      <option value="nl">Nederlands</option>
+    </select>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+
+  data() {
+    return {
+      language: "en",
+    };
+  }
+}
+</script>
