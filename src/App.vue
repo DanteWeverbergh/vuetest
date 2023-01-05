a0974fdd05c6b21575807bb2fc396d587e4f47e3
{DanteWeverbergh} <dante.weverbergh@outlook.com>
2023-01-06 00:02:19 +0100

    images

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <LanguageSelector />
+    <thumbnails />
+    <img alt="Vue logo" src="./assets/images/rotterdam.jpg" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+
 import LanguageSelector from "./components/LanguageSelector.vue";
+import Thumbnails from "./components/Thumbnails.vue";
 
 @Component({
   components: {
     LanguageSelector,
+    Thumbnails,
   },
 })
 export default class App extends Vue {}
---
+++ src/components/Thumbnails.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Thumbnails</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+
+  data() {
+    return {
+      active: "1",
+    };
+  }
+}
+</script>
